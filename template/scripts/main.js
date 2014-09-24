7fd5c7c4488b13db10dcedba67d8f5a43a2985c3
jarusified <jarus3001@gmail.com>
2014-09-24 23:47:57 +0530

    only template changes

--- template/scripts/main.js
+++ template/scripts/main.js
@@ -3,13 +3,38 @@
 	var a = document.createElement('a');
 	a.href = URL;
 
-	function render (pathname) {
-		console.log($('#'+pathname));
+
+	function contacts_start() {
+		$('.boxframe1').removeClass('openFrame');
+		$('.slidebox').css('width','100px');
+		$('img').removeClass('fullshine');
+		var thewidth = $('#firstRowImages').contents('.size1:nth-child(2)').contents().width();
+		$('.size1:nth-child(2)').width(thewidth);
+		$('.size1:nth-child(2)').contents().addClass('fullshine');
+		$('#firstRowImages').addClass('openFrame');
+	};
+	function switchimg(){
+		var thewidth = (this).width;
+		console.log(this);
+		$('.slidebox').css('width','100px');
+		$('img').removeClass('fullshine');
+		$('.boxframe1').removeClass('openFrame');
+		$(this).addClass('fullshine').parent('div').width(thewidth).parent().addClass('openFrame');
+	};
+
+
+	function render(pathname) {
 		$('#'+pathname).addClass('active',function(err){
 			if(err){
 				console.log(err);
 			}
 		});
+		if(pathname=="contacts"){
+			contacts_start();
+		}
 	}
+	var gallery=document.querySelector('.gallery');
+	console.log(gallery);
+	gallery.addEventListener('click',switchimg,false);
 	render('contacts');
 })(window.jQuery);
